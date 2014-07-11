af7c3fa4684a631b963b18d5ee0f8e40f0d02dca
Paul Biggar <paul.biggar@gmail.com>
2014-07-11 16:33:12 -0700

    Get the code and send it to the client in an unencrypted cookie

--- src/hindenbug/server.clj
+++ src/hindenbug/server.clj
@@ -7,10 +7,12 @@
             [ring.middleware.file-info :as file-info]
             [ring.middleware.resource :as resource]
             [ring.middleware.session :as session]
+            [ring.middleware.session.store :as session-store]
             [ring.util.response :as response]
             [ring.middleware.session.cookie :as cookie]
             [clj-time.core :as time]
             [clj-time.format :as time-format]
+            [cemerick.url :as url]
             [hiccup.core :as hiccup]
             [hiccup.page :as page]))
 
@@ -38,7 +40,7 @@
 
       (if (dev-mode?)
         (page/include-js "js/goog/base.js"))
-      
+
       (page/include-js "js/hindenbug.js")
       (when dev-mode?
         [:script {:type "text/javascript"} "goog.require(\"hindenbug.core\");"])]])))
@@ -47,22 +49,31 @@
   (let [response (http/post "https://github.com/login/oauth/access_token"
                             {:form-params (assoc github-client-tokens :code code)
                              :accept :json})
-        {:keys [access_token error]} (json/parse-string (:body response) true)]
-    (if (or error (not access_token))
+        {:keys [access-token error]} (json/parse-string (:body response) true)]
+    (if (or error (not access-token))
       (do
         (print error)
-        {:status 200
+        {:status 500
          :headers {"Content-Type" "text/html"}
          :body (str "Error found: " error)})
       access_token)))
 
+(deftype UnencryptedJSONCookieStore []
+  session-store/SessionStore
+  (read-session [_ data]
+    (json/decode data))
+  (write-session [_ _ data]
+    (json/encode data))
+  (delete-session [_ _]
+    (json/encode {})))
+
 (defn login [req]
   (print req)
-  (let [code (-> req :query :code)
+  (let [code (-> req :query-string url/query->map :code)
         token (fetch-github-token code)]
     {:status 302
      :headers {"Location" "/"}
-     :session {:code code}}))
+     :session {:token token :code code}}))
 
 (defn handler [req]
   (condp = (:uri req)
@@ -85,7 +96,7 @@
       file-info/wrap-file-info
       (session/wrap-session {:cookie-name "hindenbug-session"
                              :root "/"
-                             :store (cookie/cookie-store)
+                             :store (UnencryptedJSONCookieStore.)
                              :cookie-attrs {:http-only true
                                             :max-age (* 60 60 24 365)
                                             ;; expire one year after the server starts up
