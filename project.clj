8d66278cdc377afbf25c51787c3960e7fec576f7
Ian Duncan <ian@circleci.com>
2014-07-10 20:12:18 -0700

    Add core.async and set up event loop

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [com.cemerick/url "0.1.1"]
-                 [om "0.6.4"]]
+                 [om "0.6.4"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
--- src/hindenbug/core.cljs
+++ src/hindenbug/core.cljs
@@ -1,15 +1,30 @@
 (ns hindenbug.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cemerick.url :as url]))
+            [cemerick.url :as url]
+            [cljs.core.async :as async :refer [>! <! alts! chan sliding-buffer close!]])
+  (:require-macros [cljs.core.async.macros :as am :refer [go go-loop alt!]]))
 
 (enable-console-print!)
 
-(defn widget [data owner]
+(def navigation-ch
+  (chan))
+
+(def app-state
+  (atom {:channels {:navigation navigation-ch}
+         :issues {}
+         :current-team nil}))
+
+(defn team-overview [data owner]
+  (dom/h1 nil (-> data :current-team)))
+
+(defn team-listing [data owner]
   (reify
-    om/IRender
-    (render [this]
-      (dom/h1 nil (:text data)))))
+   om/IRender
+   (render [this]
+     (if-let [current-team (-> data :current-team)]
+       (team-overview [data owner])
+       (dom/h1 nil "Teams")))))
 
 (defn respond-to-oauth-code []
   (let [code (-> js/document.URL url/url :query (get "code"))
@@ -21,5 +36,15 @@
 
 (respond-to-oauth-code)
 
-(om/root widget {:text "Hello world!"}
+(om/root team-listing (app-state)
   {:target (. js/document (getElementById "app"))})
+
+(defn navigation-handler
+  [value state]
+  (print "Navigate here"))
+
+(go (while true
+      (alt!
+        navigation-ch ([v] (navigation-handler v app-state))
+        (async/timeout 10000) (do
+                                (print "Sleeping")))))
