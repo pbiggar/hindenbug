c46d4912453b9ac0ff542e761fd29dbe0d07dece
Paul Biggar <paul.biggar@gmail.com>
2014-07-10 20:00:06 -0700

    Rescue the code from the query params

--- project.clj
+++ project.clj
@@ -4,13 +4,14 @@
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2173"]
+                 [com.cemerick/url "0.1.1"]
                  [om "0.6.4"]]
 
   :plugins [[lein-cljsbuild "1.0.2"]]
 
   :source-paths ["src"]
 
-  :cljsbuild { 
+  :cljsbuild {
     :builds [{:id "hindenbug"
               :source-paths ["src"]
               :compiler {
--- src/hindenbug/core.cljs
+++ src/hindenbug/core.cljs
@@ -1,6 +1,7 @@
 (ns hindenbug.core
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [cemerick.url :as url]))
 
 (enable-console-print!)
 
@@ -9,6 +10,16 @@
     om/IRender
     (render [this]
       (dom/h1 nil (:text data)))))
+
+(defn respond-to-oauth-code []
+  (let [code (-> js/document.URL url/url :query (get "code"))
+
+        ;; bug in routing adds a '/' suffix
+        code (.substring code 0 (- (count code) 1))]
+
+    (print code)))
+
+(respond-to-oauth-code)
 
 (om/root widget {:text "Hello world!"}
   {:target (. js/document (getElementById "app"))})
