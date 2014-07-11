9c62c58b275fca69cf1dfb681eb25258e587ee77
Paul Biggar <paul.biggar@gmail.com>
2014-07-10 20:47:55 -0700

    Update to latest cljs and clojure and lein-cljsbuild

--- project.clj
+++ project.clj
@@ -2,14 +2,14 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2268"]
                  [com.cemerick/url "0.1.1"]
                  [secretary "1.2.1-SNAPSHOT"]
                  [om "0.6.4"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
   :source-paths ["src"]
 
