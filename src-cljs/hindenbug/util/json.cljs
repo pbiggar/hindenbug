539b5181f5106373ca6e2eed144132cab3c89610
Paul Biggar <paul.biggar@gmail.com>
2014-07-12 19:09:44 -0700

    Use more idiomatic form

--- src-cljs/hindenbug/util/json.cljs
+++ src-cljs/hindenbug/util/json.cljs
@@ -3,9 +3,9 @@
 (defn generate-string
   "Turn a clojurescript value into a JSON one."
   [val]
-  (.stringify js/JSON (clj->js val)))
+  (-> val clj->js js/JSON.stringify))
 
 (defn parse
   "Parse a JSON value into a clojurescript one."
   [string]
-  (js->clj (.parse js/JSON string)))
+  (-> string js/JSON.parse (js->clj :keywordize-keys false))) ; TODO change to true
