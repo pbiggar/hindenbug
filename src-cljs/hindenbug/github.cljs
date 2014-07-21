7747fe85d6676eb86bfaa38ef1e79d9c4326e183
Paul Biggar <paul.biggar@gmail.com>
2014-07-20 18:01:13 -0700

    Move defaults into the options arg

--- src-cljs/hindenbug/github.cljs
+++ src-cljs/hindenbug/github.cljs
@@ -2,17 +2,17 @@
   (:require [hindenbug.tent.core :as tent :include-macros true]
             [hindenbug.tent.issues :as issues]
             [hindenbug.tent.search :as search]
-            [hindenbug.login :as login]))
+            [hindenbug.login :as login])
+  (:require-macros [hindenbug.utils :refer (inspect)]))
 
 
 (defn callback [context response]
   (js/alert "callback"))
 
 (defn issue [number & {:as context}]
-  (tent/with-defaults {:callback callback
-                       :calling-context context
-                       :oauth_token (login/oauth-token)}
-    (issues/issue "circleci" "hindenbug-manual-test" number)))
+  (issues/specific-issue "circleci" "hindenbug-manual-test" number {:callback callback
+                                                                    :calling-context context
+                                                                    :oauth-token (inspect (login/oauth-token))}))
 
 (defn issue-search [term & {:as context}]
   nil)
